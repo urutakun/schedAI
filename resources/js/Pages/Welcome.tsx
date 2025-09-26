1b257b7ba2d94a2dcf5e8f9d7d5195abd7d8c5b4
mngoplt <waltergagate1001@gmail.com>
2025-09-26 21:17:04 +0800

    feat(first_commit): Hello schedAI

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import { PageProps } from '@/types';
+import { Head, Link } from '@inertiajs/react';
+
+export default function Welcome() {
+
+    return (
+      <div className='bg-black h-screen w-screen'>
+        <h1 className='text-red-500 text-4xl'>WADAPAK</h1>
+      </div>
+    );
+}
